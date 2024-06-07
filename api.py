a93f3a55620f732851de4c276f9fee0c860165fb
fedlallah mohamed yacine <fed.myacine@gmail.com>
2024-06-07 03:22:28 +0200

    setting the api

--- api.py
+++ api.py
@@ -17,3 +17,20 @@ def apiFirst():
     except (KeyError, IndexError, ValueError,requests.exceptions.HTTPError,requests.exceptions.ConnectionError):
         return None
 
+
+
+def apiSearch(query):
+    url = f"http://ip-api.com/json/{query}"
+
+    try:
+        response = requests.get(
+            url,
+            cookies={"session": str(uuid.uuid4())},
+            headers={"Accept": "*/*", "User-Agent": "python-requests"},
+        )
+        response.raise_for_status()
+
+        result = response.json()
+        return result
+    except (KeyError, IndexError, ValueError,requests.exceptions.HTTPError,requests.exceptions.ConnectionError):
+        return None
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, redirect, render_template,request
 from ipaddress import is_global
+from api import apiFirst, apiSearch
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     ip = request.args.get("ip")
     if not ip:
-        return render_template("layout.html")
-    elif is_global(ip):
+        ip1st = apiFirst()
+        ipRes = apiSearch(ip1st["ip"])
+        return render_template("layout.html",api = ipRes)
+    else:
         return render_template("layout.html")
